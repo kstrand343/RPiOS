63d629fec43ce34c302980679c2fa6dfb80d3ca5
kstrand343 <k_strandell@comcast.net>
2016-04-20 15:54:13 -0400

    Update timer.h

--- timer.h
+++ timer.h
@@ -1,6 +1,15 @@
 #include <stdint.h>
-#includ "mem_access.h"
-#include "rpi_peripherals.h"
+#include "mem_access.h"
+
+#define SYS_TIMER		      0x20003000UL
+
+#define SYS_TIMER_CS		  SYS_TIMER
+#define SYS_TIMER_CLO		  0x20003004UL
+#define SYS_TIMER_CHI		  0x20003008UL
+#define SYS_TIMER_C0		  0x2000300CUL
+#define SYS_TIMER_C1		  0x20003010UL
+#define SYS_TIMER_C2		  0x20003014UL
+#define SYS_TIMER_C3		  0x20003018UL
 
 void wait(uint32_t duration)
 {

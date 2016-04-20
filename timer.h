7234b93738f7006e749c2ab4d958e86514c3ca13
kstrand343 <k_strandell@comcast.net>
2016-04-20 15:52:24 -0400

    Update timer.h

--- timer.h
+++ timer.h
@@ -2,4 +2,8 @@
 #includ "mem_access.h"
 #include "rpi_peripherals.h"
 
-wait(uint32_t millisec);
+void wait(uint32_t duration)
+{
+  volatile uint32_t check = mem_read(SYS_TIMER_CLO) + duration;
+  while(mem_read(SYS_TIMER_CLO) < check);
+}

ee7cc76644adf30348c547cba408170ab428d258
kstrand343 <k_strandell@comcast.net>
2016-04-20 15:15:12 -0400

    Create kernel.c

---
+++ kernel.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include "mem_access.h"
+
+int main()
+{
+  while(1)
+  {
+    // Turn on LED_ACT
+    // Turn off LED_PWR
+    // Wait 2 seconds
+    // Turn off LED_ACT
+    // Turn on LED_PWR
+    // Wait 1 second
+  }
+  return -1
+}

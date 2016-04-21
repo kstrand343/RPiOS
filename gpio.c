8181a2e287d14a6c5b25ae35822e5394610fcdc0
kstrand343 <k_strandell@comcast.net>
2016-04-21 10:47:55 -0400

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -8,29 +8,35 @@ uint32_t gpio_set_function(uint32_t pin, uint32_t function)
 uint32_t gpio_pin_enable(uint32_t pin)
 {
   uint32_t* reg_gpset;
+  uint32_t set_bit;
   
   if(pin >=0 && pin < 32){
     reg_gpset = (uint32_t*) GPSET0;
-    bit = pin;
+    set_bit = 31 - pin;
     }
   else if (pin >=32 && pin < 54){
-    reg_gpset = (uint32_t*) GPSET1;bit = (pin-32);
+    reg_gpset = (uint32_t*) GPSET1;
+    set_bit = (pin-32);
     }
+    
+  *reg_gpset = (1 << set_bit);
 }
 
 uint32_t gpio_pin_disable(uint32_t pin)
 {
   uint32_t* reg_gpclr;
-  uint32_t bit;
+  uint32_t clr_bit;
   
   if(pin >=0 && pin < 32){
     reg_gpclr = (uint32_t*) GPCLR0;
-    bit = pin;
+    clr_bit = 31 - pin;
     }
   else if (pin >=32 && pin < 54){
     reg_gpclr = (uint32_t*) GPCLR1;
-    bit = (pin-32);
+    clr_bit = (pin-32);
     }
+    
+  *reg_gpclr = (1 << clr_bit);
 
   
 }
